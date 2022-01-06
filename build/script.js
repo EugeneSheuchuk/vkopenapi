cfae5ef4787e305b1de08c0a599fabe27d5c619c
Eugene Sheuchuk <e_sheuchuk@twistellar.com>
2022-01-06 16:02:16 +0300

    add get posts logic

--- build/script.js
+++ build/script.js
@@ -1,10 +1,21 @@
 console.log('VK ', VK);
 
+let userId;
+
 VK.init({ apiId: 8042738 });
 
 const authBtn = document.getElementById('auth');
+const getPostsBtn = document.getElementById('gPosts');
+
 authBtn.addEventListener('click', function () {
     VK.Auth.login(function(response) {
         console.log('response ', response);
+        userId = response.user.id;
     }, 270338);
+});
+
+getPostsBtn.addEventListener('click', function() {
+    VK.Api.call('wall.get', { owner_id: userId, v:"5.73" }, function(r) {
+        console.log('R', r);
+    });
 });
