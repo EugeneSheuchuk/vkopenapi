e50a3437d03d42bd7d1f8da483ec08523805969e
Eugene Sheuchuk <e_sheuchuk@twistellar.com>
2022-01-06 15:51:29 +0300

    add login logic

--- build/script.js
+++ build/script.js
@@ -1,3 +1,10 @@
 console.log('VK ', VK);
 
 VK.init({ apiId: 8042738 });
+
+const authBtn = document.getElementById('auth');
+authBtn.addEventListener('click', function () {
+    VK.Auth.login(function(response) {
+        console.log('response ', response);
+    }, 270338);
+});
