fed7dd6b00a564dd95765cd6ef19a17ef8c40af2
Eugene Sheuchuk <e_sheuchuk@twistellar.com>
2022-01-06 16:21:06 +0300

    add get reposts logic ver 1

--- build/script.js
+++ build/script.js
@@ -6,6 +6,7 @@ VK.init({ apiId: 8042738 });
 
 const authBtn = document.getElementById('auth');
 const getPostsBtn = document.getElementById('gPosts');
+const searchRepostBtn = document.getElementById('sRepost');
 
 authBtn.addEventListener('click', function () {
     VK.Auth.login(function(response) {
@@ -15,7 +16,17 @@ authBtn.addEventListener('click', function () {
 });
 
 getPostsBtn.addEventListener('click', function() {
+    console.log('Get Posts ');
     VK.Api.call('wall.get', { owner_id: userId, v:"5.131" }, function(r) {
+        console.log('R', r);
+    });
+});
+
+searchRepostBtn.addEventListener('click', function() {
+    console.log('Get Reposts ');
+    const postId = +document.getElementById('postId').value;
+    console.log('postId ', postId);
+    VK.Api.call('wall.getReposts', { owner_id: userId, v:"5.131", getReposts: postId }, function(r) {
         console.log('R', r);
     });
 });
